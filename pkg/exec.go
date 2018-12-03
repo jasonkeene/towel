9c39a02bf0d69dfdefe5a7d4d75aa3eb9fc12e16
Jason Keene <jasonkeene@gmail.com>
2018-12-02 17:44:32 -0700

    allow pods names in exec cmd

--- pkg/exec.go
+++ pkg/exec.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/spf13/pflag"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
@@ -14,6 +15,7 @@ import (
 type flags struct {
 	fs *pflag.FlagSet
 
+	podName       string
 	labelSelector string
 	fieldSelector string
 	namespace     string
@@ -26,7 +28,45 @@ func parseFlags(args []string) (*flags, error) {
 	f.fs.StringVarP(&f.labelSelector, "selector", "l", "", "")
 	f.fs.StringVar(&f.fieldSelector, "field-selector", "", "")
 	f.fs.StringVarP(&f.namespace, "namespace", "n", "default", "")
-	return f, f.fs.Parse(args)
+	err := f.fs.Parse(args)
+	if err != nil {
+		return nil, err
+	}
+	f.podName = f.fs.Arg(0)
+
+	return f, nil
+}
+
+func podInfo(kubeClient *kubernetes.Clientset, f *flags) (string, string, error) {
+	var pod *corev1.Pod
+	if f.podName != "" {
+		var err error
+		pod, err = kubeClient.CoreV1().Pods(f.namespace).Get(f.podName, metav1.GetOptions{})
+		if err != nil {
+			return "", "", err
+		}
+	} else {
+		pl, err := kubeClient.CoreV1().Pods(f.namespace).List(metav1.ListOptions{
+			LabelSelector: f.labelSelector,
+			FieldSelector: f.fieldSelector,
+		})
+		if err != nil {
+			return "", "", err
+		}
+		if len(pl.Items) < 1 {
+			return "", "", fmt.Errorf("there are no pods with that search criteria: %#v", f)
+		}
+		if len(pl.Items) > 1 {
+			return "", "", fmt.Errorf("there are too many pods with that search criteria: %#v", f)
+		}
+		pod = &pl.Items[0]
+	}
+	nodeName := pod.Spec.NodeName
+	containerID := strings.TrimPrefix(
+		pod.Status.ContainerStatuses[0].ContainerID,
+		"docker://",
+	)
+	return nodeName, containerID, nil
 }
 
 func Exec(kubeClient *kubernetes.Clientset, args ...string) error {
@@ -35,26 +75,12 @@ func Exec(kubeClient *kubernetes.Clientset, args ...string) error {
 		return err
 	}
 
-	pl, err := kubeClient.CoreV1().Pods(f.namespace).List(metav1.ListOptions{
-		LabelSelector: f.labelSelector,
-		FieldSelector: f.fieldSelector,
-	})
+	nodeName, containerID, err := podInfo(kubeClient, f)
 	if err != nil {
 		return err
 	}
-	if len(pl.Items) < 1 {
-		return fmt.Errorf("there are no pods with that search criteria: %#v", f)
-	}
-	if len(pl.Items) > 1 {
-		return fmt.Errorf("there are too many pods with that search criteria: %#v", f)
-	}
-	nodeName := pl.Items[0].Spec.NodeName
-	containerID := strings.TrimPrefix(
-		pl.Items[0].Status.ContainerStatuses[0].ContainerID,
-		"docker://",
-	)
 
-	pl, err = kubeClient.CoreV1().Pods("default").List(metav1.ListOptions{
+	pl, err := kubeClient.CoreV1().Pods("default").List(metav1.ListOptions{
 		LabelSelector: "app=towel",
 		FieldSelector: "spec.nodeName=" + nodeName,
 	})
